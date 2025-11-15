b24f8434ff1d4da55c088698e04c794946166f27
Punkyherisson <emmanuel.goitia@gmail.com>
2025-11-15 09:34:10 +0100

    Niveau 3 et bonnes decisions

---
+++ Faire les bonnes decisions avec les courses.clj
@@ -0,0 +1,30 @@
+Faire les bonnes decisions avec les courses
+Tout commence au moment des achats : si vous achetez des snacks des chocolats vous finirez par les manger
+Il faut faire les bons choix au niveau des achats
+utiliser une liste de course est associee a un regime sain
+Faire une liste de courses avec les bons aliments bien repartis va vous aider a vous concentrer 
+sur ce dont vous avez besoin et ne pas etre tenté 
+Ecrire une liste avant de partir (regarder frigos et placards)
+Ne pas faire les courses en ayant faim
+favoriser des produits bruts avec peu d'ingredients (moins de 54 si possible)
+choisir des fruits frais et locaux et de saison
+lisez bien les etquettes
+Organiser sa liste par groupes d'aliments
+proteines (viandes, poissons, oeufs, legumes secs)
+glucides (cereales completes, pain complet, riz complet, patates douces)
+lipides (huiles vegetales, oléagineux, avocat)
+fibres (fruits, legumes, graines)
+hydrater (eau, tisanes)*
+eviter les produits transformes, plats prepares, snacks, sodas, sucreries
+Faire les courses de maniere strategique
+aller faire les courses apres un repas
+faire les courses seul(e)
+prendre son temps
+commencer par les rayons fruits et legumes
+terminer par les produits frais (viandes, poissons, produits laitiers)
+eviter les rayons sucreries, biscuits, snacks
+ne pas se laisser tenter par les promotions sur les produits malsains
+Respecter son plan alimentaire a la maison
+faire ses repas a la maison
+preparer ses repas a l'avance
+eviter de grignoter entre les repas

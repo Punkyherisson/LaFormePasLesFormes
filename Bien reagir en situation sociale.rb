2e6bf9dfcb13dcfdce7bba2e44f4c9d55e3daf8d
Punkyherisson <emmanuel.goitia@gmail.com>
2025-11-16 10:25:00 +0100

    Motivation et Methode Binome

---
+++ Bien reagir en situation sociale.rb
@@ -0,0 +1,17 @@
+Bien reagir en situation sociale
+Boucle de l'habitude
+agir sur les situations qui rompent avec vos habitudes.
+Pour beaucoup, suivre de nouvelles habitudes alimentaires est difficile dans des situations comme :
+un repas de famille
+un apertif entre amis.
+un anniversaire, une sortie ..
+Quand je suis au restaurant avec des amis
+jai envie de boire et manger 
+donc je bois de l'alcool et je grignote
+alors je passe une bonne soiree
+exemple sortie bureau 
+supprimer le declencheur
+rendre la mauvaise habitude difficile
+remplacer la mauvaise habitude par une bonne habitude.
+exemple : au restaurant, je commande une entree legere et un verre d'eau gazeuse avec du citron
+je bois un verre d'eau avant de partir

757d049550155f7e5024d9dad80e4a2a85e06970
Punkyherisson <emmanuel.goitia@gmail.com>
2025-11-17 08:27:23 +0100

    Decisions courses et sorties

---
+++ Faire les bonnes decisions avec les cour.groovy
@@ -0,0 +1,30 @@
+Faire les bonnes decisions avec les courses
+Faire les bons choix au restaurant ou prenez un repas a emporter 
+se poser la question de quand on mange a lexterieur
+- au restaurant d'entreprise
+    * ne prenez pas de decision trop rapide
+    * boire de l'eau avant
+    * face au buffet essayez de composer assiette 25% prot 25% feculent 50% legumes
+    * eviter le pain 
+    * yaourt et fruit plutot que dessert
+    * choisir un plat avec proteine
+    * plat avec feculent et legume
+    * limiter l'alcool
+    * partager le dessert avec qqun
+il est plus simple de suivre uin regime si vous preparez ce que vous mangez
+l'ideal est de cuisiner soi meme
+si derapge il faut compenser au repas suivant
+je sais que lorsque je mange au restaurant
+jai envie de prendre du pain
+donc je ne prends pas de pain a la maison
+Faire une photo du plat avant de manger
+prendre le temps de manger
+manger en pleine conscience
+eviter les distractions (telephone, tv)
+manger lentement
+poser les couverts entre chaque bouchée
+savourer chaque bouchée
+ecouter les signaux de satiété
+arreter de manger avant d'etre plein
+
+

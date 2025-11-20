73822e7a40fadff0d0e1960cdec7c4b4e20e7772
Punkyherisson <emmanuel.goitia@gmail.com>
2025-11-20 09:01:11 +0100

    Poids et Mensurations

---
+++ Prendre les bonnes decisions en cuisine.clj
@@ -0,0 +1,16 @@
+Prendre les bonnes decisions en cuisine
+Comment preparer les proteines
+Comment cuire les viandes/poissons : au four ou en grillade (pas beacoup de graisse)
+quelle matiere grasse utiliser : 1-2 cuillere a cafe huile olive (eviter le beurre car acides gras saturés)
+et eviter margarine. pour les oeufs, cuire les blancs et laisser les jaunes crus (oeuf poches coque ou plat)
+Comment cuire les legumes : a la vapeur ou a letouffee (pour garder vitamines et mineraux)+1cc huile olive
+la cuisson a l'eau est a eviter car perte vitamines et mineraux .. la cuisson au micro onde est a eviter aussi
+preferer la cuisson a la vapeur et au four.
+Pour les feculents pates et riz à l'eau bouillante salée (1 litre d'eau + 10g sel par 100g de pates ou riz)
+evitez les plats qui demandent de repasser le riz à la poele.
+Les pommes de terre : cuisson a l'eau ou au four (eviter friture)
+Les salades : utiliser huile olive + vinaigre balsamique ou jus de citron
+Les sauces : eviter sauces cremes et bechamel, preferer sauces tomates maison ou sauces a base de yaourt nature
+Les desserts : fruits frais de saison, yaourt nature, fromage blanc 0% remplacer le beurre par du fromage blanv=c ou de la courgette cuite
+couper farine blanche avec farine complete ou farine d'epautre
+ 

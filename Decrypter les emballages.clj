1155c6723807851b6e242f2c00df4389038a706c
Punkyherisson <emmanuel.goitia@gmail.com>
2025-11-21 08:42:45 +0100

    Lire les etiquettes

---
+++ Decrypter les emballages.clj
@@ -0,0 +1,23 @@
+Decrypter les emballages
+Savoir decrypter les etiquettes permet deviter de faire des erreurs alimentaires
+Grace a ce cours vous aller pouvoir verifier vos connaissances et en acquerir de nouvelles
+La liste des ingredients est la seule information obligatoire
+Que dois je regarder ?
+La composition: les ingredients sont classés par ordre decroissant de poids.
+Le premier se trouve en haut, en quantité.
+Preferer les aliments avec moins de % de sucre, moins de matieres grasses, le moins d'ingredients possibles, preferez les produits avec les ingredients que vous connaissez
+En regardant la composition en macroelements on peut faire le bon choix
+Les valeurs nutritionnelles
+Energie (kcal ou kJ)
+... comparer les valeurs par portion et par 100g
+favoriser les aliments avec le moins d'acides gras saturés (cholesterol)
+favoriser les aliments avec le moins de sel, le plus de fibres, le plus de proteines
+faire des exercices avec des vrais etiquettes et les comparer !!!
+Le Nutriscore ?
+ABCDE - il est basé sur un algorithme qui prend en compte les elements a favoriser et ceux a limiter
+Cest un bon indicateur mais il ne faut pas se fier uniquement a lui 
+Il ne prend pas en compte les additifs, les allergenes, le mode de production (bio ou pas)
+Il est preferable de regarder la composition en detail
+
+c'est simple d'utiliser le code a barre !
+Scanner le code a barre avec une application mobile (Yuka, Open Food Facts, etc)

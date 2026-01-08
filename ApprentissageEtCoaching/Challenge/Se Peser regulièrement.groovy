9499cab946e54e4c9437f93099847043471304fd
Punkyherisson <emmanuel.goitia@gmail.com>
2026-01-08 08:28:43 +0100

    Challenge et pesée

---
+++ ApprentissageEtCoaching/Challenge/Se Peser regulièrement.groovy
@@ -0,0 +1,21 @@
+Se Peser regulièrement
+Et noter les resultats est important pour regarder ou on en est par rapport à l'objectif
+Normalement au quotidien sinon une fois par semaine
+aujourd'hui le 08/01/2026, j'ai 94 et 27.5 de Masse Corporelle
+Faire le questionnaire du poids au bout de 6 jours si rien noté dans l'application log
+Exemple: quelle est votre dernière pesee ? Combien pesez vous maintenant?
+Comment mon poids a evolué
+Si le poids stagne : peut etre normal car la perte de poids n'est pas lineaire et on peut avoir deconné avec les fetes par Exemple
+Il faut aussi regarder les autres indicateurs comme le sommeil, les niveaux d'energie et de motivation, les mensuraions et l'autoevaluation
+
+Si le poids ne baisse pas cela peut etre du a plusieurs facteurs :
+regime pas adapté ... trop restrictif -> tendance a deconner
+on prend trop de glucides et pas assez le legumes/proteines
+Activité physique insuffisante
+stress, sommeil insuffisant
+hydration insuffisante
+Il est important de faire le point sur ces facteurs et d'ajuster son regime et son mode de vie en consequence
+Il est aussi important de se rappeler que la perte de poids durable prend du temps et qu'il faut etre patient et persévérant dans ses efforts.
+Il est conseillé de consulter un professionnel de la santé ou un nutritionniste pour obtenir des conseils personnalisés et adaptés à sa situation individuelle.
+Il faut garder en tete l'objectif global de santé et de bien-être plutot que de se focaliser uniquement sur le chiffre sur la balance.
+Il est aussi important de célébrer les progrès réalisés, meme s'ils sont petits, et de rester motivé pour continue.
